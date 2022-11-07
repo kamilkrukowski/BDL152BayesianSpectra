bb13a006cdcbf478ae27ec0a7233f1525e190ac2
Kamil Krukowski <kamiljkrukowski@gmail.com>
2022-11-07 08:52:06 -0500

    ~0.15 CS with deeper network

--- vanilla_ffn.py
+++ vanilla_ffn.py
@@ -17,33 +17,39 @@ class Vanilla_FFN(pl.LightningModule):
         super().__init__()
 
         INPUT_SIZE = 167
-        OUTPUT_SIZE = 1,000
+        OUTPUT_SIZE = 1000
 
         self.ffn = nn.Sequential(
-            nn.Linear(INPUT_SIZE, 16384), nn.ReLU(), 
-            nn.Linear(16384, 8096), nn.ReLU(),
-            nn.Linear(8096, 2048), nn.ReLU(),
-            nn.Linear(2048, 1000), nn.ReLU()
+            nn.Linear(INPUT_SIZE, 4096), nn.ReLU(), 
+            nn.Linear(4096, 4096), nn.ReLU(),
+            nn.Linear(4096, 4096), nn.ReLU(),
+            nn.Linear(4096, 4096), nn.ReLU(),
+            nn.Linear(4096, 4096), nn.ReLU(),
+            nn.Linear(4096, OUTPUT_SIZE), nn.ReLU()
 
                                     )
-        
-        self.loss = F.mse_loss
-
     def forward(self, x):
         return self.ffn(x)
 
+    def get_loss(self, y_hat, y, mask=None):
+        loss = F.mse_loss(y_hat, y, reduction='none')
+        if mask is not None:
+            loss *= mask;
+            return loss.sum() / mask.sum();
+        return loss.mean();
+
     def training_step(self, batch, batch_idx):
-        x, y = batch
+        x, y, mask = batch
         y_hat = self.forward(x)
-        loss = self.loss(y_hat, y)
+        loss = self.get_loss(y_hat, y)
         
         self.log("train_loss", loss)
         return loss
 
     def validation_step(self, batch, batch_idx):
-        x, y = batch
+        x, y, mask = batch
         y_hat = self.forward(x)
-        loss = self.loss(y_hat, y)
+        loss = self.get_loss(y_hat, y)
         
         self.log("test_loss", loss)
         self.log("test_cosine_sim", F.cosine_similarity(y_hat, y).mean(), prog_bar=True)
@@ -57,14 +63,14 @@ class Vanilla_FFN(pl.LightningModule):
         return optimizer
 
 
-TRAIN_BATCH_SIZE = 1024
+TRAIN_BATCH_SIZE = 8192
 
 model = Vanilla_FFN()
 
 dataset = MoNADataset()
-train_set, test_set, extra = utils.data.random_split(dataset, [0.8, 0.2, 0.00], generator=Generator().manual_seed(2022))
+train_set, test_set, extra = utils.data.random_split(dataset, [0.2, 0.05, 0.75], generator=Generator().manual_seed(2022))
 
-train_loader = utils.data.DataLoader(train_set, num_workers=12, batch_size=1024) 
+train_loader = utils.data.DataLoader(train_set, num_workers=12, batch_size=TRAIN_BATCH_SIZE) 
 test_loader = utils.data.DataLoader(test_set, batch_size=len(test_set)) 
 
 trainer = pl.Trainer(max_epochs=5, auto_select_gpus = True, auto_scale_batch_size=True)
