Fixed off-by-one, peakRank calculation, sped up test_vi.py

ffn.py
@@ -52,8 +52,8 @@ class FFN(pl.LightningModule):
         return self.ffn(x)
 
     def get_loss(self, y_hat, y, mask=None):
-        #loss = 1 - F.cosine_similarity(y_hat, y, axis=1)
-        loss = F.cross_entropy(y_hat, np.argmax(y, axis=1)).mean()
+        loss = 1 - F.cosine_similarity(y_hat, y, axis=1)
+        #loss = F.cross_entropy(y_hat, np.argmax(y, axis=1)).mean()
         return loss.mean();
     
     def get_metrics(self, y_hat, y, log_name):

mean_field_vi.py
@@ -45,11 +45,13 @@ class BayesianLinear(nn.Module):
         # Weight parameters
         self.weight_mu = nn.Parameter(torch.Tensor(out_features, in_features).uniform_(-0.2, 0.2))
         self.weight_rho = nn.Parameter(torch.randn((out_features, in_features)))
+        #self.weight_rho = torch.Tensor(0.01*torch.ones((out_features, in_features)))
         self.weight = Gaussian(self.weight_mu, self.weight_rho)
 
         # Bias parameters
         self.bias_mu = nn.Parameter(torch.Tensor(out_features).uniform_(-0.2, 0.2))
         self.bias_rho = nn.Parameter(torch.randn((out_features)))
+        #self.bias_rho = torch.Tensor(0.01*torch.ones((out_features)))
         self.bias = Gaussian(self.bias_mu, self.bias_rho)
 
         # Prior distribution
@@ -141,9 +143,9 @@ class BayesianNetwork(pl.LightningModule):
         # Cosine similarity between (un)normalized peaks and model output 
         self.log(f"cosineSim/{log_name}", F.cosine_similarity(y_hat, y).mean(), prog_bar=True)
         # Mean AUROC of top-1 peak vs all other peaks across molecules
-        self.log(f"mAUROC/{log_name}", np.mean([sklearn.metrics.roc_auc_score(F.one_hot(np.argmax(y[i]), self.OUTPUT_SIZE).reshape(-1,1), y_hat[i]) for i in range(len(y))]), prog_bar=True)
+        self.log(f"mAUROC/{log_name}", np.mean([sklearn.metrics.roc_auc_score(F.one_hot(np.argmax(y[i]), self.OUTPUT_SIZE), y_hat[i]) for i in range(len(y))]), prog_bar=True)
         # Mean top-1 peak Rank across molecules
-        self.log(f"peakRank/{log_name}", np.mean([float(i) for i in np.argmax(y_hat, axis=1)]), prog_bar=True)
+        self.log(f"peakRank/{log_name}", np.mean([np.argsort(y_hat[i].detach().numpy())[np.argmax(y[i])] for i in range(len(y))]), prog_bar=True)
 
     def validation_step(self, batch, batch_idx):
         x, y, *_ = batch
@@ -178,7 +180,7 @@ if __name__ == '__main__':
     test_loader = utils.data.DataLoader(test_set, num_workers=num_workers, batch_size=TEST_BATCH_SIZE) 
 
     METRIC = 'mAUROC/val'
-    TRIAL_DIR = 'logs/mfvi1'
+    TRIAL_DIR = 'logs/mfvi2'
     checkpoint_callback = pl.callbacks.ModelCheckpoint(
         TRIAL_DIR, monitor=METRIC, mode='max', filename='best'
     )
@@ -189,8 +191,10 @@ if __name__ == '__main__':
     # Remove previous Tensorboard statistics
     if os.path.exists(f"{TRIAL_DIR}/lightning_logs/version_0"):
         os.system(f"rm -r {TRIAL_DIR}/lightning_logs/version_0")
+        os.system(f"rm -r {TRIAL_DIR}/best.ckpt")
 
-    model = BayesianNetwork(lr=1e-3, hidden_layer_sizes=[512,512,512], input_size=len(train_set[0][0]), output_size=1000, samples=8)
+    model = BayesianNetwork(lr=1e-3, hidden_layer_sizes=[4096], input_size=len(train_set[0][0]),
+                            output_size=1000, samples=1, q_sigma=10.0)
 
     trainer = pl.Trainer(max_epochs=EPOCHS, auto_select_gpus = True, auto_scale_batch_size=True,
                             callbacks=callbacks, logger=logger)

test_vi.py
@@ -20,47 +20,75 @@ model = BayesianNetwork.load_from_checkpoint(f"{log_dir}/best.ckpt", lr=1e-3, hi
                                              input_size=len(train_set[0][0]), output_size=1000, samples=8)
 
 num_workers = multiprocessing.cpu_count()
-TEST_BATCH_SIZE=len(test_set)
+TEST_BATCH_SIZE = 1024
 
-test_loader = utils.data.DataLoader(test_set, num_workers=num_workers, batch_size=TEST_BATCH_SIZE) 
+test_loader = utils.data.DataLoader(test_set, num_workers=num_workers, batch_size=TEST_BATCH_SIZE, drop_last=True) 
 
-def get_calibration(conf, posterior_pred, y):
+def get_calibration(conf, posterior_pred, y, add=0.0):
         # First find confidence interval bounds
-        lower_bound = np.percentile(posterior_pred, (0.5-(conf/2.0))*100, axis=2)
-        upper_bound = np.percentile(posterior_pred, (0.5+(conf/2.0))*100, axis=2)
+        lower_bound = np.percentile(posterior_pred, (0.5-(conf/2.0))*100, axis=1)
+        upper_bound = np.percentile(posterior_pred, (0.5+(conf/2.0))*100, axis=1) + add
 
         print(posterior_pred.shape)
         
         # Find confidence interval containment by logical AND on upper and lower bounds surrounding the true value
         lower_true = np.less_equal(lower_bound, y)
-        print(f"lower_true at {conf:0.2f} is {np.sum(lower_true):0.2f}")
+        print(f"bounded_below by {(0.5-(conf/2.0)):0.2f} is {np.sum(lower_true):0.2f}")
         upper_true = np.greater_equal(upper_bound, y)
-        print(f"upper_true at {conf:0.2f} is {np.sum(upper_true):0.2f}")
+        print(f"bounded_above by {(0.5+(conf/2.0)):0.2f} is {np.sum(upper_true):0.2f}")
         contained = np.logical_and(lower_true, upper_true)
         
         calibration = np.mean(contained)
-        print(f"contained in {conf:0.2f} is {np.sum(contained):0.2f}")
+        print(f"contained in {conf:0.2f}-interval is {np.sum(contained):0.2f}")
         print(f"Calibration (interval) at {conf:0.2f} is {calibration:0.2f}")
         return calibration
     
 
-n_samples = 16
-posterior_pred = None
-posterior_pred1 = None
 y = None
+n_samples = 16
+# Monte Carlo Posterior Predictive
+tracked = []
+peaks = []
 for (x, y, *_) in test_loader:
+    
+    y = y.detach().numpy()
+    batch_size = y.shape[0]
+    posterior_pred = list() 
+    
+    # Select highest peaks
     indices = np.argmax(y, axis=1)
-    y = y.detach().numpy()[indices]
+    peaks.append(indices)
     
-    # Monte Carlo Posterior Predictive
-    posterior_pred1 = []
     for i in range(n_samples):
+         
         y_hat = model.forward(x, sample=True).detach().numpy()
-        posterior_pred1.append(y_hat)
-    posterior_pred = np.stack(posterior_pred1, axis=2)
+        
+        _posterior_sample = np.exp(y_hat)
+            
+        posterior_pred.append(_posterior_sample)
+
+    posterior_pred = np.stack(posterior_pred, axis=2)
+   
+    # Predictive Posterior Estimate 
+    ppe = np.mean(posterior_pred, axis=2)
+
+    bounds = []
+    for conf in [0.5]:
+        # First find confidence interval bounds
+        lower_bound = np.percentile(posterior_pred, (0.5-(conf/2.0))*100, axis=2)
+        upper_bound = np.percentile(posterior_pred, (0.5+(conf/2.0))*100, axis=2)
+        bounds.append((lower_bound, upper_bound))
+
+    tracked.append((ppe, bounds))
     
-    # We only consider calibration on highest peak
-    posterior_pred = posterior_pred[indices]
-    
-    for conf in [0.99]:
-        get_calibration(conf, posterior_pred, y)
+peaks2 = np.stack(peaks, axis=1).reshape(-1)
+
+ppe = np.stack([i[0] for i in tracked], axis=0).reshape(-1, 1000)
+bounds = list()
+for i in range(len(tracked[0][1])):
+    print(np.stack([j[1][i][0] for j in tracked], axis=0).shape)
+    l_bounds = np.stack([j[1][i][0] for j in tracked], axis=0).reshape(-1,1000)
+    u_bounds = np.stack([j[1][i][1] for j in tracked], axis=0).reshape(-1,1000)
+    bounds.append((l_bounds, u_bounds))
+
+def get_rank(scores, peaks2)